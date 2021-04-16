153dbfe05bf78c612121a5db8dc618b7a5f2285e
scyzh6 <scyzh6@nottingham.edu.cn>
2021-04-16 16:56:51 +0800

    Update test script

---
+++ src/CW3/src/BoCCategoryTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoCCategoryTest {
+
+    @Test
+    void categoryName() {
+    }
+
+    @Test
+    void categoryBudget() {
+    }
+
+    @Test
+    void categorySpend() {
+    }
+
+    @Test
+    void setCategoryName() {
+    }
+
+    @Test
+    void setCategoryBudget() {
+    }
+
+    @Test
+    void addExpense() {
+    }
+
+    @Test
+    void removeExpense() {
+    }
+
+    @Test
+    void resetBudgetSpend() {
+    }
+
+    @Test
+    void getRemainingBudget() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
