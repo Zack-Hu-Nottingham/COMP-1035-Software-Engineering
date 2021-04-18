74b4cf838176f34e4dca3762038f4b5073e9b89d
scyzw10 <scyzw10@nottingham.edu.cn>
2021-04-18 20:33:38 +0800

    Added transCate getter

--- src/CW3/test/BoCTransactionTest.java
+++ src/CW3/test/BoCTransactionTest.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +20,14 @@ class BoCTransactionTest {
     void transactionValue() {
     }
 
-    @Test
-    void transactionCategory() {
+    // Author: Ziyi Wang
+    // Last modified: 2021/4/18
+    @ParameterizedTest
+    @CsvFileSource(resources = { "/Trans_getCategory.csv" })
+    void transactionCategory(int input, int expectation) {
+        final BoCTransaction tCate = new BoCTransaction("wzy-hzx", new BigDecimal("2000"), input);
+        int num = tCate.transactionCategory();
+        assertEquals(expectation,num);
     }
 
     @Test
