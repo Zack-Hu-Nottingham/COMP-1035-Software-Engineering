7acc30bba03faec79cca5b9bdce6d31b54aaa402
Yingxiao Huo <scyyh9@nottingham.edu.cn>
2021-04-18 21:10:47 +0800

    add authors and last modified time.

--- src/CW3/test/BoCTransactionTest.java
+++ src/CW3/test/BoCTransactionTest.java
@@ -1,19 +1,34 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoCTransactionTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    //author: Yingxiao Huo
+    //last modified on； 2021/4/18
     @Test
-    void transactionName() {
-    	final BoCTransaction Test_getter = new BoCTransaction();
-        final Field field_getter = Test_getter.getClass().getDeclaredField("transactionName");
+    void transactionName() throws NoSuchFieldException, IllegalAccessException {
+        final BoCTransaction Test_getter = new BoCTransaction();
+        final Field field_getter = Test_getter.getClass().getDeclaredField("test0");
         field_getter.setAccessible(true);
         field_getter.set(Test_getter, "test0");
         final String result = (String) Test_getter.transactionName();
-        assertEquals("test0", result, "field wasn't retrieved properly");
+        assertEquals("field wasn't retrieved properly", result, "test0");
+
     }
 
     @Test
@@ -28,8 +43,18 @@ class BoCTransactionTest {
     void transactionTime() {
     }
 
-    @Test
-    void setTransactionName() {
+    //author: Yicun Duan
+    //Last modified on: 2021/4/18
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/setNameTest.csv"})
+    void setTransactionName(String name) throws NoSuchFieldException, IllegalAccessException {
+        final BoCTransaction test = new BoCTransaction();
+
+        test.setTransactionName(name);
+
+        final Field field = test.getClass().getDeclaredField("transactionName");
+        field.setAccessible(true);
+        assertEquals(name, field.get(test));
     }
 
     @Test
@@ -44,9 +69,11 @@ class BoCTransactionTest {
     void setTransactionTime() {
     }
 
+    //author: Yingxiao Huo
+    //Last modified on: 2021/4/18
     @ParameterizedTest
     @CsvFileSource(resources = {"/toStringTest.csv"})
-    	void testToString(String transName, String transValue, String resultStr) throws NoSuchFieldException, IllegalAccessException {
+    void testToString(String transName, String transValue, String resultStr) throws NoSuchFieldException, IllegalAccessException {
         final BoCTransaction Test_toString = new BoCTransaction();
         final Field test1 = Test_toString.getClass().getDeclaredField("transactionName");
         final Field test2 = Test_toString.getClass().getDeclaredField("transactionValue");
