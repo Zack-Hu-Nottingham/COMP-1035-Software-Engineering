58999a0c4d2d6400204de500a60cd0e591ee9aca
sqylt2 <sqylt2@nottingham.edu.cn>
2021-04-18 19:16:57 +0800

    Upload test for Default Constructor and Getter

--- src/CW3/test/BoCTransactionTest.java
+++ src/CW3/test/BoCTransactionTest.java
@@ -5,13 +5,36 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoCTransactionTest {
+    @Test
+    void BoCTransaction() throws NoSuchFieldException, IllegalAccessException{
+        final BoCTransaction boc = new BoCTransaction();
+        final Field fieldName = boc.getClass().getDeclaredField("transactionName");
+        final Field fieldValue = boc.getClass().getDeclaredField("transactionValue");
+        final Field fieldCategory = boc.getClass().getDeclaredField("transactionCategory");
+        final Field fieldTime = boc.getClass().getDeclaredField("transactionTime");
+        fieldName.setAccessible(true);
+        fieldValue.setAccessible(true);
+        fieldCategory.setAccessible(true);
+        fieldTime.setAccessible(true);
+        assertEquals("[Pending Transaction]", fieldName.get(boc),"Field transactionName didn't match");
+        assertEquals(null, fieldValue.get(boc),"Field transactionValue didn't match");
+        assertEquals(0, fieldValue.get(boc),"Field transactionCategory didn't match");
+        assertEquals(null, fieldValue.get(boc),"Field transactionTime didn't match");
+    }
 
     @Test
     void transactionName() {
     }
 
     @Test
-    void transactionValue() {
+    void transactionValue() throws  NoSuchFieldException, IllegalAccessException{
+        final BoCTransaction boc = new BoCTransaction();
+        final Field field = boc.getClass().getDeclaredField("transactionValue");
+        BigDecimal expectValue = new BigDecimal(1);
+        field.setAccessible(true);
+        field.set(boc, expectValue);
+        final BigDecimal result = boc.transactionValue();
+        assertEquals( expectValue, result, "Field transactionValue wasn't retrieved properly");
     }
 
     @Test
