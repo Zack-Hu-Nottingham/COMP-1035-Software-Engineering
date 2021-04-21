fa7b138546d73eaeb37b431e306f63b41282c255
Yingxiao <scyyh9@nottingham.edu.cn>
2021-04-21 06:48:12 -0700

    Upload test for BoCcategory name getter and setter

--- src/CW3/test/BoCCategoryTest.java
+++ src/CW3/test/BoCCategoryTest.java
@@ -1,11 +1,24 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoCCategoryTest {
 
-    @Test
-    void categoryName() {
+    //author: Yingxiao Huo
+    //Last Modified time: 2021/4/21
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/categoryNameGetter.csv"})
+    void categoryName(String name, String expection) throws NoSuchFieldException, IllegalAccessException{
+        final BoCCategory Test_getter = new BoCCategory();
+        final Field field_getName = Test_getter.getClass().getDeclaredField("CategoryName");
+        field_getName.setAccessible(true);
+        field_getName.set(Test_getter, name);
+        final String result = (String) Test_getter.CategoryName();
+        assertEquals(result, expection);
     }
 
     @Test
@@ -16,8 +29,16 @@ class BoCCategoryTest {
     void categorySpend() {
     }
 
-    @Test
-    void setCategoryName() {
+    //author: Yingxiao Huo
+    //Last modified time: 2021/4/21
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/categoryNameSetter.csv"})
+    void setCategoryName(String name, String expection) throws NoSuchFieldException, IllegalAccessException{
+        final BoCCategory Test_setter = new BoCCategory();
+        Test_setter.setCategoryName(name);
+        final Field field_setname = Test_setter.getClass().getDeclaredField("CategoryName");
+        field_setname.setAccessible(true);
+        assertEquals(name, field_setname.get(Test_setter));
     }
 
     @Test
