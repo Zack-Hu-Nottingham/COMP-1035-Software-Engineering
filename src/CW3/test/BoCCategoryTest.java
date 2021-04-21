a22f792c555690efb3ecec5182fcaadfc46b1bbd
linchenn <biylc2@nottingham.edu.cn>
2021-04-21 17:58:41 +0800

    Update test script for resetBudgetSpend function of BoCCategory

--- src/CW3/test/BoCCategoryTest.java
+++ src/CW3/test/BoCCategoryTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoCCategoryTest {
@@ -32,8 +35,18 @@ class BoCCategoryTest {
     void removeExpense() {
     }
 
+    //Author:LinCHEN(biylc2)
+    //Last Modify:2021/04/21
     @Test
-    void resetBudgetSpend() {
+    void resetBudgetSpend() throws NoSuchFieldException, IllegalAccessException {
+        BoCCategory reset1= new BoCCategory("Reset");
+        Field field1=reset1.getClass().getDeclaredField("CategorySpend");
+        field1.setAccessible(true);
+        field1.set(reset1,new BigDecimal("20000.0292"));
+        reset1.resetBudgetSpend();
+        BigDecimal result1= (BigDecimal) field1.get(reset1);
+        assertEquals(new BigDecimal("0.00"),result1);
+
     }
 
     @Test
