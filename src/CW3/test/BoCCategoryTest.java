f522e92f1d0e29eae7cca47add3a974ac2d68d0b
sqylt2 <sqylt2@nottingham.edu.cn>
2021-04-22 10:52:55 +0800

    Update test for category default constructor and Report.md

--- src/CW3/test/BoCCategoryTest.java
+++ src/CW3/test/BoCCategoryTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
