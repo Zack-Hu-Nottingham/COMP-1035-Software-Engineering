294a3ace500b24147a78a9dfb0f80936a1c759eb
Yingxiao <scyyh9@nottingham.edu.cn>
2021-04-22 01:12:23 -0700

    Fix mistakes

--- src/CW3/test/BoCCategoryTest.java
+++ src/CW3/test/BoCCategoryTest.java
@@ -3,10 +3,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoCCategoryTest {
+
+    // Author: Leshan Tan
+    // Last Modified: 2021/4/21
+    @Test
+    void BocCategory()throws NoSuchFieldException, IllegalAccessException{
+        final BoCCategory boc = new BoCCategory();
+        final Field fieldName = boc.getClass().getDeclaredField("CategoryName");
+        final Field fieldBudget = boc.getClass().getDeclaredField("CategoryBudget");
+        final Field fieldSpend = boc.getClass().getDeclaredField("CategorySpend");
+        fieldName.setAccessible(true);
+        fieldBudget.setAccessible(true);
+        fieldSpend.setAccessible(true);
+        assertEquals("New Category", fieldName.get(boc),"Field CategoryName didn't match");
+        assertEquals(new BigDecimal("0.00"),fieldBudget.get(boc),"Field CategoryBudget didn't match");
+        assertEquals(new BigDecimal("0.00"), fieldSpend.get(boc),"Field CategorySpend didn't match");
+    }
 
     //author: Yingxiao Huo
     //Last Modified time: 2021/4/21
@@ -38,9 +55,9 @@ class BoCCategoryTest {
         Test_setter.setCategoryName(name);
         final Field field_setname = Test_setter.getClass().getDeclaredField("CategoryName");
         field_setname.setAccessible(true);
-        assertEquals(name, field_setname.get(Test_setter));
+        assertEquals(expection, field_setname.get(Test_setter));
     }
-
+    
     @Test
     void setCategoryBudget() {
     }
