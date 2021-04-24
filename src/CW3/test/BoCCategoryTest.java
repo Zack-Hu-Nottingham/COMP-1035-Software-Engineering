16fb76b1ae77157e562a066f71530e87c8572015
scyzw10 <scyzw10@nottingham.edu.cn>
2021-04-24 23:24:27 +0800

    Modified TestScript for BoCCate

--- src/CW3/test/BoCCategoryTest.java
+++ src/CW3/test/BoCCategoryTest.java
@@ -163,14 +163,14 @@ class BoCCategoryTest {
 
     // Author: Ziyi Wang
     // Last modified: 2021/4/23 19:31
-//    @Test
-//    void setCategoryBudget(){
-//        final BoCCategory stest = new BoCCategory();    //new object
-//
-//        float input = 20.00F;
-//        //argument passed to setCategoryBudget is incompatible with the type expected
-//        stest.setCategoryBudget(input);    // set the budget with float type
-//    }
+    @Test
+    void setCategoryBudget(){
+        final BoCCategory stest = new BoCCategory();    //new object
+
+        float input = 20.00F;
+        //argument passed to setCategoryBudget is incompatible with the type expected
+        stest.setCategoryBudget(input);    // set the budget with float type
+    }
 
     // Author : LinCHEN(biylc2)
     // Last Modify: 2021/04/24
