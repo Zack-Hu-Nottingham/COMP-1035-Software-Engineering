30e94a215ae6ab57182bdc64aadb0b61d292c211
linchenn <biylc2@nottingham.edu.cn>
2021-04-28 01:26:33 +0800

    Modify list trans

--- src/CW3/src/BoCApp.java
+++ src/CW3/src/BoCApp.java
@@ -88,7 +88,7 @@ public class BoCApp {
 	public static void ListTransactions() {
 		for (int x = 0; x < UserTransactions.size(); x++) {
 			BoCTransaction temp = UserTransactions.get(x);
-			System.out.println((x + 1) + ") " + temp.transactionName()+"("+);
+			System.out.println((x + 1) + ") " + temp.transactionName()+" ("+UserCategories.get(temp.transactionCategory()).CategoryName()+") - ¥"+temp.transactionValue());
 		}
 	}
 
